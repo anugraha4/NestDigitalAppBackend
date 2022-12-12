712578469020f3538b3b3a4c01a58ef2a5b9304e
anugraha4 <96745531+anugraha4@users.noreply.github.com>
2022-12-12 15:33:30 +0530

    dao

--- NestDigitalApp/src/main/java/com/example/NestDigitalApp/controller/EmployeeController.java
+++ NestDigitalApp/src/main/java/com/example/NestDigitalApp/controller/EmployeeController.java
@@ -1,25 +1,33 @@
 package com.example.NestDigitalApp.controller;
 
+import com.example.NestDigitalApp.dao.EmployeeDao;
 import com.example.NestDigitalApp.model.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.List;
+
 @RestController
 public class EmployeeController {
+
+    @Autowired
+    private EmployeeDao dao;
     @GetMapping("/")
     public String MainPage()
     {return "Welcome to main page";}
 
 
-    @PostMapping("/login")
-    public String login()
-    {return "login page";}
+    @PostMapping(path="/login",consumes = "application/json",produces = "application/json")
+    public String login(@RequestBody Employee e)
+   {
+        return "login page";}
 
     @PostMapping(path="/add",consumes = "application/json",produces = "application/json")
-    public String EmpAdd(@RequestBody Employee e)
-    {
+    public HashMap<String,String> EmpAdd(@RequestBody Employee e) {
         System.out.println(e.getEmpcode());
         System.out.println(e.getName().toString());
         System.out.println(e.getDesignation().toString());
@@ -27,17 +35,24 @@ public class EmployeeController {
         System.out.println(e.getPhnNo().toString());
         System.out.println(e.getMail().toString());
         System.out.println(e.getUsername().toString());
-        return "Employee Added successfully";}
+        dao.save(e);
+        HashMap<String, String> map = new HashMap<>();
+        map.put("status", "success");
+        return map;
+    }
 
-    @PostMapping("/search")
-    public String EmpSearch()
-    {return "search done";}
+    @PostMapping(path="/search",consumes = "application/json",produces = "application/json")
+    public List<Employee> EmpSearch(@RequestBody Employee e) {
+        String empcode = String.valueOf(e.getEmpcode());
+        System.out.println(e.getEmpcode());
+        return (List<Employee>) dao.SearchEmployee(e.getEmpcode());
+    }
 
     @PostMapping("/delete")
     public String EmpDelete()
     {return "delete done";}
 
     @GetMapping("/viewprofile")
-    public String EmpViewProf()
-    {return " Profile";}
+    public List<Employee> EmpViewProf()
+    {return (List<Employee>) dao.findAll();}
 }
